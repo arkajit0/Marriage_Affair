6938e28189f7a3b8735e78f7e809005e429b4a05
Arkajit Majumder <arkajeet.majumder@gmail.com>
2021-03-12 02:52:46 +0530

    workflow addesd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def base():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST', 'GET'])
 def index():
@@ -51,8 +51,7 @@ def index():
         # return render_template("test.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host="0.0.0.0")
 
 
 
